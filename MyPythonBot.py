13ac1b6487a563f04a9a5937669f8e19fc32ed13
Dilray <115149242+Dilray@users.noreply.github.com>
2025-01-08 10:48:39 +0300

    Big changes

--- MyPythonBot.py
+++ MyPythonBot.py
@@ -1,41 +1,47 @@
-# coding=utf-8
-import telebot
-import webbrowser
-
+import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from config import TOKEN
 
-bot = telebot.TeleBot(TOKEN)
+# Включаем логирование
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Start
-@bot.message_handler(commands=['hello', 'start', 'привет', 'старт']) # можно добавить несколько команд
-def main(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\nДобро пожаловать в лучшего бота для скачивания контента из интернета. Что ж, начнём?', parse_mode='html')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Привет! Отправь мне ссылку на видео с YouTube, и я пришлю его тебе.')
 
-# Help
-@bot.message_handler(commands=['help', 'помощь'])
-def help(message):
-    bot.send_message(message.chat.id, '<b>Помощь с использованием:</b>\n' +
-                    'Список команд\n' +
-                    '<i>/start</i> - начать пользоваться ботом\n' +
-                    '<i>/site</i> - открыть ссылку на мой сайт (allArticles)\n' +
-                    '<i>/id</i> - узнать ваш id в Telegram\n' +
-                    '<i>/help</i> - помощь в навигации по боту', parse_mode='html')
+def download_video(url: str) -> str:
+    from pytube import YouTube
+    yt = YouTube(url)
+    video = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+    
+    if not video:
+        video = yt.streams.get_lowest_resolution()
 
-# Перекидывает на мой сайт
-@bot.message_handler(commands=['site', 'website', 'сайт', 'вебсайт', 'веб-сайт'])
-def site(message):
-    webbrowser.open('https://all-articles.onrender.com/')
+    video_file_path = video.download(output_path='downloads')
+    return video_file_path
 
-# Выдаёт айдишник юзера
-@bot.message_handler(commands=['id', 'ID', 'Id', 'iD'])
-def id(message):
-    bot.reply_to(message, f'Твой ID в Telegram: <b>{message.from_user.id}</b>', parse_mode='html')
 
-# @bot.message_handler()
-# def info(message):
-#     if message.text.lower == 'приветы':
-#         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\nДобро пожаловать в лучшего бота для скачивания контента из интернета. Что ж, начнём?', parse_mode='html')
-#     else:
-#         bot.send_message(message.chat.id, 'Не понял твоего сообщения! Давай лучше общаться командами.')
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    url = update.message.text
+    if 'youtube.com/watch' in url or 'youtu.be/' in url:
+        try:
+            await update.message.reply_text('Скачиваю видео, подождите...')
+            video_file_path = download_video(url)
+            await update.message.reply_video(video=open(video_file_path, 'rb'))
+            os.remove(video_file_path)  # Удаляем файл после отправки
+        except Exception as e:
+            await update.message.reply_text(f'Произошла ошибка: {e}')
+    else:
+        await update.message.reply_text('Пожалуйста, отправьте действительную ссылку на видео с YouTube.')
 
-bot.polling(none_stop=True)
+def main() -> None:
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))  # Обрабатываем все текстовые сообщения
+
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
